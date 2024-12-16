b05fffc0c3637ed30b390f5809c07866688ef8c7
Harsha Runwal <106861144+Harsha-Runwal@users.noreply.github.com>
2024-12-16 18:08:18 +0530

    project-1 files added

---
+++ rockwell/server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var path=require("path");
+var fs=require("fs");
+
+
+//server configuration
+app.use(express.static(path.join(__dirname,'public')));
+
+ app.get('/', function (req, res) {
+   res.sendFile(path.join(___dirname + '/index.html'));
+});
+ 
+
+app.get('/hello', function (req, res) {
+  console.log("--CALLING rest api old--");
+  var person={firstName:'- Harsha ',lastName:'Runwal'};
+  res.send(person);
+});
+
+ app.listen(9459, '0.0.0.0', () => {
+    console.log('Example V-1.0 app is listening on http://0.0.0.0:9459');
+});
+// var server = app.listen(9459, function () {
+//   var host = server.address().address
+//   var port = server.address().port
+//   console.log("host = ",host)
+//  console.log("port = ",port)
+//   console.log("Example app is listening at http://localhost:9459", host, port)
+// })
